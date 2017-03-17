6b50829daca5ad2362956fc50a1d312613837588
Kelly Johnson <kellyjohnson@Kellys-MacBook-Pro.local>
2017-03-16 21:53:32 -0700

    Initial Commit

---
+++ TrueFalseStarter/TriviaDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  TriviaDataModel.swift
+//  TrueFalseStarter
+//
+//  Created by Michael Guatambu Davis on 3/16/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+struct TriviaSource {
+
+
+    let triviaQuestions: [[String : String]] = [
+        ["Question": "What is a Berimbau?", "Answer": "A musical Instrument"],
+        ["Question": "What is a Queixada?", "Answer": "A circular kick"],
+        ["Question": "What is a Cabeçada?", "Answer": "A head butt"],
+        ["Question": "What is an Aú?", "Answer": "A cartwheel"]
+    ]
+
+}
---
+++ TrueFalseStarter/ViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ViewController.swift
+//  TrueFalseStarter
+//
+//  Created by Pasan Premaratne on 3/9/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+import GameKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion: Int = 0
+    let triviaSource = TriviaSource()
+    var gameSound: SystemSoundID = 0
+ 
+    /*
+    let triviaQuestions: [[String : String]] = [
+        ["Question": "What is a Berimbau?", "Answer": "A musical Instrument"],
+        ["Question": "What is a Queixada?", "Answer": "A circular kick"],
+        ["Question": "What is a Cabeçada?", "Answer": "A head butt"],
+        ["Question": "What is an Aú?", "Answer": "A cartwheel"]
+    ]
+ */
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var false2Button: UIButton!
+    @IBOutlet weak var false3Button: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadGameStartSound()
+        // Start game
+        playGameStartSound()
+        displayQuestion()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func displayQuestion() {
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: triviaSource.triviaQuestions.count)
+        let questionDictionary = triviaSource.triviaQuestions[indexOfSelectedQuestion]
+        questionField.text = questionDictionary["Question"]
+        playAgainButton.isHidden = true
+    }
+    
+    func displayScore() {
+        // Hide the answer buttons
+        trueButton.isHidden = true
+        falseButton.isHidden = true
+        false2Button.isHidden = true
+        false3Button.isHidden = true
+        
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+        
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        // Increment the questions asked counter
+        questionsAsked += 1
+        
+        let selectedQuestionDict = triviaSource.triviaQuestions[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestionDict["Answer"]
+        
+        if (sender === trueButton &&  correctAnswer == "A musical Instrument") || (sender === falseButton && correctAnswer == "A circular kick") || (sender === false2Button && correctAnswer == "A head butt") ||
+            (sender === false3Button && correctAnswer == "A cartwheel") {
+            correctQuestions += 1
+            questionField.text = "Correct!"
+        } else {
+            questionField.text = "Sorry, wrong answer!"
+        }
+        
+        loadNextRoundWithDelay(seconds: 2)
+    }
+    
+    func nextRound() {
+        if questionsAsked == questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // Continue game
+            displayQuestion()
+        }
+    }
+    
+    @IBAction func playAgain() {
+        // Show the answer buttons
+        trueButton.isHidden = false
+        falseButton.isHidden = false
+        false2Button.isHidden = false
+        false3Button.isHidden = false
+        
+        questionsAsked = 0
+        correctQuestions = 0
+        nextRound()
+    }
+    
+
+    
+    // MARK: Helper Methods
+    
+    func loadNextRoundWithDelay(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
+    
+    func loadGameStartSound() {
+        let pathToSoundFile = Bundle.main.path(forResource: "GameSound", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: pathToSoundFile!)
+        AudioServicesCreateSystemSoundID(soundURL as CFURL, &gameSound)
+    }
+    
+    func playGameStartSound() {
+        AudioServicesPlaySystemSound(gameSound)
+    }
+}
+
